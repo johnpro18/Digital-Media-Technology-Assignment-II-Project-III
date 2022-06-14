8bde8c4088dfbd70952460f1a50b50489550a235
johnpro18 <derptrollerguy@gmail.com>
2022-06-14 23:01:31 +0800

    14/6/2022 Version 3

--- Assets/Scripts/Collectibles/CoinCollectibles.cs
+++ Assets/Scripts/Collectibles/CoinCollectibles.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class CoinCollectibles : MonoBehaviour
 {
     [SerializeField] private int coinValue = 1;
+    [SerializeField] private AudioClip audio;
 
     public CoinsCounter coinsCounter;
-
+    
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.tag == "Player")
         {
             coinsCounter.SetCoinsCounter(coinValue);
+            AudioSource.PlayClipAtPoint(audio, transform.position);
             gameObject.SetActive(false);
         }
     }
